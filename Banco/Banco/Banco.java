e577d941b4336047ebac45a857ad31cf7e856ed0
Dio Lemos <pedro.devsc@gmail.com>
2024-05-17 01:06:07 -0300

    Adicionar atributo nome a banco, getters, setters e atualizar toString

--- Banco/Banco/Banco.java
+++ Banco/Banco/Banco.java
@@ -8,13 +8,29 @@ import Cliente.Cliente;
 
 public class Banco {
      private List<Cliente> clientes;
+     public String nome;
      
    private static final int[] AGENCIAS = {0001, 0002, 0003, 0004, 0005, 0006};
 
-      public Banco() {
+      public Banco(String nome) {
         this.clientes = new ArrayList<>();
+        setNome(nome);
         
     }
+    public void setNome(String nome) {
+        if(nome.length()>=4 && nome.length()<=12 && nome != null){
+            this.nome = nome;
+        }
+        else{
+            throw new IllegalArgumentException("Nome precisa ter no mínimo 4 letras e no máximo 12");
+        }
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+
     public List<Cliente> getClientes() {
         return clientes;
     }
@@ -29,8 +45,8 @@ public class Banco {
 
     @Override
     public String toString() {
-        return "Banco{" +
-                "clientes=" + clientes +
+        return "Banco{" + nome +
+                " clientes=" + clientes +
                 ", Agências=" + Arrays.toString(AGENCIAS) +
                 '}';
     }
